5c577618028b96af5a863f24db1adcabac33db0d
Beilong <bt132@duke.edu>
2024-11-05 10:24:03 +0800

    delete download.sh

--- download.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p ckpt
-
-echo "Downloading hifi-ckpt in ckpt folder:"
-wget -P ckpt https://github.com/bshall/knn-vc/releases/download/v0.1/g_02500000.pt
-# echo "Downloading detokenizer conformer in ckpt folder:"
-# wget -P ckpt https://github.com/bshall/knn-vc/releases/download/v0.1/g_02500000.pt
-
-
