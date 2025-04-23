6f9be8200b734b3877f81c281cab47c8d01fe63d
Beilong <bt132@duke.edu>
2025-04-23 08:46:58 +0800

    update code

--- kmeans/train_kmeans_all_spks_librispeech.sh
+++ kmeans/train_kmeans_all_spks_librispeech.sh
@@ -4,8 +4,8 @@
 ## CONFIG: librispeech base path and wavlm ckpt ##
 ##################################################
 
-base_path=/SMIIPdata3/zbang/Corpus/librispeech/LibriSpeech
-wavlm_ckpt=/DKUdata/tangbl/wavlm/WavLM-Large.pt
+base_path=/SMIIPdata3/zbang/Corpus/librispeech/LibriSpeech # LibriSpeech base_path
+wavlm_ckpt=/DKUdata/tangbl/wavlm/WavLM-Large.pt # WavLM Path
 
 ########################################
 # Exp1: 10 randomly select 10 speakers #
@@ -24,16 +24,17 @@ wavlm_ckpt=/DKUdata/tangbl/wavlm/WavLM-Large.pt
 ######################################################
 ## Exp2: run Kmeans on the whole librispeech dataset #
 ######################################################
-config=exp/librispeech_multiple/config/k_1024.yaml
-ckpt_dir=exp/librispeech_multiple/ckpt/librispeech_k_1024_config_all
-
 ### DDP 
-num_proc=24
+num_proc=6
 gpus="cuda:1 cuda:2 cuda:3 cuda:4 cuda:6 cuda:7"
 
 ## Data Path
 tr_360=$(ls /SMIIPdata3/zbang/Corpus/librispeech/LibriSpeech/train-clean-360 | tr '\n' ' ') # train-clean-360
 tr_100=$(ls /SMIIPdata3/zbang/Corpus/librispeech/LibriSpeech/train-clean-100 | tr '\n' ' ') # train-clean-100
+
+config=exp/librispeech_multiple/config/k_1024.yaml
+ckpt_dir=exp/librispeech_multiple/ckpt/librispeech_k_1024_config_all
+
 spks="$tr_360 $tr_100"
 python -u kmeans/train_kmeans_librispeech.py --base_path $base_path \
     --spk_list $spks --config $config --wavlm_ckpt $wavlm_ckpt \
