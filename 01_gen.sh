887a215fac7ed39ae46fa5004f72b5bd6d8c40fb
xiaoxiaomiao323 <152830384+xiaoxiaomiao323@users.noreply.github.com>
2025-06-12 13:46:41 +0800

    Update 01_gen.sh

--- 01_gen.sh
+++ 01_gen.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
+export CUDA_VISIBLE_DEVICES=4
 
-for dset in  libri_dev_{enrolls,trials_f,trials_m} \
-		libri_test_{enrolls,trials_f,trials_m} \
-		IEMOCAP_dev IEMOCAP_test train-clean-360; do
+flag=_spkall
+out=anon_speech_mkf
+conformer=ckpt/librispeech_conformer_e_50.pth
+
+for dset in  libri_dev_enrolls libri_test_enrolls train-clean-360; do
 
 
-#python inference_audio.py --audio_scp data/$dset/wav.scp --output_dir anon_speech/$dset/  --kmeans_path ckpt/LJSpeech/kmeans-cluster-1024-k_1024.pt --ckpt_path ckpt/LJSpeech/LJSpeech_k_1024_model.pt --config ckpt/LJSpeech/k_1024.yaml
-python inference_audio.py --audio_scp data/$dset/wav.scp --output_dir anon_speech/$dset/  --kmeans_path ./ckpt/MSP-IMPROV/MSP_IMPROV_kmeans-cluster-1024-k_1024.pt --ckpt_path ./ckpt/MSP-IMPROV/MSP-Improv_k_1024_model.pt
+python exp/multiple_kmeans_train_one_conformer/inference_audio_multi_kmeans.py \
+    --audio_scp data/$dset/wav.scp \
+    --output_dir $out/$dset \
+    --kmeans_scp  kmeans$flag.scp \
+    --ckpt $conformer \
+
+done
+
+flag=_libri_every_20
+
+for dset in  libri_dev_{trials_f,trials_m} \
+		libri_test_{trials_f,trials_m} \
+		IEMOCAP_dev IEMOCAP_test; do
+
+python exp/multiple_kmeans_train_one_conformer/inference_audio_multi_kmeans.py \
+    --audio_scp data/$dset/wav.scp \
+    --output_dir $out/$dset \
+    --kmeans_scp  kmeans$flag.scp \
+    --ckpt $conformer \
 
 done
